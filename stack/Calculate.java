0f750179ed53d5fa0f8c5c2442f20b5f07afc13e
jinch <jinchunyu86@outlook.com>
2023-11-28 20:00:25 +0800

    stack

--- stack/Calculate.java
+++ stack/Calculate.java
@@ -7,23 +7,28 @@ import java.util.Deque;
  */
 public class Calculate {
 	public int calculate(String s) {
-		Deque<Integer> numDeque = new ArrayDeque<>();
+		s = s.replace(" ", "");
+		Deque<Long> numDeque = new ArrayDeque<>();
 		Deque<Character> opDeque = new ArrayDeque<>();
+		if(s.startsWith("+")||s.startsWith("-")){
+			numDeque.push(0L);
+		}
 		for (int i = 0; i < s.length(); i++) {
-			if(' '==s.charAt(i)){
-				continue;
-			}
 			if (Character.isDigit(s.charAt(i))) {
-				int sum = s.charAt(i) - '0';
+				long sum = s.charAt(i) - '0';
 				while (i+1 < s.length() && Character.isDigit(s.charAt(i+1))){
 					sum = sum *10 + (s.charAt(i+1)-'0');
 					i++;
 				}
 				numDeque.push(sum);
 				while (!opDeque.isEmpty() && ('+' == opDeque.peek() || '-' == opDeque.peek())) {
-					Character pop = opDeque.pop();
-					Integer pop1 = numDeque.pop();
-					Integer pop2 = numDeque.pop();
+					char pop = opDeque.pop();
+					Long pop1 = numDeque.pop();
+					Long pop2 = 0L;
+					if(!numDeque.isEmpty()){
+						pop2 = numDeque.pop();
+					}
+
 					if ('+' == pop) {
 						numDeque.push(pop2 + pop1);
 					}
@@ -34,16 +39,40 @@ public class Calculate {
 			}
 			else if (')' != s.charAt(i)) {
 				opDeque.push(s.charAt(i));
+				if('(' == s.charAt(i)){
+					if(i+1 < s.length() && ('+' == s.charAt(i+1) || '-'==s.charAt(i+1))){
+						numDeque.push(0L);
+						opDeque.push(s.charAt(++i));
+					}
+				}
+
 			}
 			else if (')' == s.charAt(i)) {
 				opDeque.pop();
+				while (!opDeque.isEmpty() && ('+' == opDeque.peek() || '-' == opDeque.peek())) {
+					char pop = opDeque.pop();
+					Long pop1 = numDeque.pop();
+					Long pop2 = 0L;
+					if(!numDeque.isEmpty()){
+						pop2 = numDeque.pop();
+					}
+					if ('+' == pop) {
+						numDeque.push(pop2 + pop1);
+					}
+					else if ('-' == pop) {
+						numDeque.push(pop2 - pop1);
+					}
+				}
 			}
 		}
 
 		while (!opDeque.isEmpty() && ('+' == opDeque.peek() || '-' == opDeque.peek())) {
-			Character pop = opDeque.pop();
-			Integer pop1 = numDeque.pop();
-			Integer pop2 = numDeque.pop();
+			char pop = opDeque.pop();
+			Long pop1 = numDeque.pop();
+			Long pop2 = 0L;
+			if(!numDeque.isEmpty()){
+				pop2 = numDeque.pop();
+			}
 			if ('+' == pop) {
 				numDeque.push(pop2 + pop1);
 			}
@@ -52,12 +81,12 @@ public class Calculate {
 			}
 		}
 
-		return numDeque.pop();
+		return Math.toIntExact(numDeque.pop());
 	}
 
 	public static void main(String[] args) {
 		Calculate calculate = new Calculate();
-		int calculate1 = calculate.calculate("2147483647+1");
+		int calculate1 = calculate.calculate("- (3 + (4 + 5))");
 		System.out.println(calculate1);
 	}
 }
